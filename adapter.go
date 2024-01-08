d0a6303767537e97fa9ae97d953bc0d72bc58341
sted <stefano.delliponti@icloud.com>
2024-01-08 19:28:09 +0100

    Middleware adapters

--- adapter.go
+++ adapter.go
@@ -23,6 +23,9 @@ func (w *AdapterResponseWriter) Status() int {
 	return w.status
 }
 
+// Adapt adapts a standard http.HandlerFunc to be used as a Heligo handler.
+// In the standard handler one can retrieve parameters from the context,
+// using ParamsFromContext.
 func Adapt(hf http.HandlerFunc) Handler {
 	return func(ctx context.Context, w http.ResponseWriter, r Request) (int, error) {
 		rw := &AdapterResponseWriter{w, http.StatusOK}
@@ -37,6 +40,43 @@ func Adapt(hf http.HandlerFunc) Handler {
 	}
 }
 
+// ParamsFromContext gets the route parameters from the context
 func ParamsFromContext(ctx context.Context) []Param {
-	return ctx.Value(ParamsTag).([]Param)
+	v := ctx.Value(ParamsTag)
+	if v == nil {
+		return nil
+	}
+	return v.([]Param)
+}
+
+// AdaptMiddleware adapts a standard middleware (func(http.Handler) http.Handler)
+// to be used as a Heligo middleware
+func AdaptMiddleware(m func(http.Handler) http.Handler) Middleware {
+	return func(next Handler) Handler {
+		return func(ctx context.Context, w http.ResponseWriter, r Request) (int, error) {
+			h := http.HandlerFunc(func(ww http.ResponseWriter, rr *http.Request) {
+				next(rr.Context(), ww, Request{Request: rr})
+			})
+			rw := &AdapterResponseWriter{w, http.StatusOK}
+			req := r.Request
+			m(h).ServeHTTP(rw, req)
+			return rw.Status(), nil
+		}
+	}
+}
+
+// AdaptAsMiddleware adapts a standard http.HandlerFunc to be used as
+// a Heligo middleware. Note the this is less flexible than the previous
+// AdaptMiddleware, as it doesn't not allow to break the middleware chain
+// except in the case of errors and calls the next handler only at the end.
+func AdaptAsMiddleware(hf http.HandlerFunc) Middleware {
+	return func(next Handler) Handler {
+		return func(ctx context.Context, w http.ResponseWriter, r Request) (int, error) {
+			s, err := Adapt(hf)(ctx, w, r)
+			if err != nil {
+				return s, err
+			}
+			return next(ctx, w, r)
+		}
+	}
 }
--- adapter_test.go
+++ adapter_test.go
@@ -2,6 +2,7 @@ package heligo_test
 
 import (
 	"bytes"
+	"context"
 	"io"
 	"net/http"
 	"net/http/httptest"
@@ -24,9 +25,9 @@ func TestAdapter(t *testing.T) {
 	if w.Result().StatusCode != http.StatusTeapot {
 		t.Fail()
 	}
-	body, _ := io.ReadAll(w.Result().Body)
+	content, _ := io.ReadAll(w.Result().Body)
 	w.Result().Body.Close()
-	if !bytes.Equal(body, []byte("Test adapter")) {
+	if !bytes.Equal(content, []byte("Test adapter")) {
 		t.Fail()
 	}
 }
@@ -35,7 +36,11 @@ func TestAdapterParams(t *testing.T) {
 	router := heligo.New()
 	standardHandler := func(w http.ResponseWriter, r *http.Request) {
 		params := heligo.ParamsFromContext(r.Context())
-		if len(params) != 3 ||
+		if r.URL.Path == "/adapt/none" {
+			if len(params) != 0 {
+				t.Fail()
+			}
+		} else if len(params) != 3 ||
 			params[0].Name != "one" || params[0].Value != "1" ||
 			params[1].Name != "two" || params[1].Value != "2" ||
 			params[2].Name != "three" || params[2].Value != "3/45" {
@@ -43,7 +48,73 @@ func TestAdapterParams(t *testing.T) {
 		}
 	}
 	router.Handle("GET", "/adapt/:one/:two/*three", heligo.Adapt(standardHandler))
+	router.Handle("GET", "/adapt/none", heligo.Adapt(standardHandler))
 	w := httptest.NewRecorder()
 	r, _ := http.NewRequest("GET", "/adapt/1/2/3/45", nil)
 	router.ServeHTTP(w, r)
+
+	w = httptest.NewRecorder()
+	r, _ = http.NewRequest("GET", "/adapt/none", nil)
+	router.ServeHTTP(w, r)
+}
+
+func TestMiddlewareAdapter(t *testing.T) {
+	router := heligo.New()
+	router.Use(heligo.AdaptMiddleware(func(next http.Handler) http.Handler {
+		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			if r.URL.Path == "/adapt/m1" {
+				w.WriteHeader(http.StatusTeapot)
+			} else {
+				next.ServeHTTP(w, r)
+			}
+		})
+	}))
+	h := func(ctx context.Context, w http.ResponseWriter, r heligo.Request) (int, error) {
+		return 200, nil
+	}
+	router.Handle("GET", "/adapt/*m", h)
+	w := httptest.NewRecorder()
+
+	r, _ := http.NewRequest("GET", "/adapt/m1", nil)
+	router.ServeHTTP(w, r)
+	if w.Result().StatusCode != http.StatusTeapot {
+		t.Fail()
+	}
+
+	w = httptest.NewRecorder()
+	r, _ = http.NewRequest("GET", "/adapt/m2", nil)
+	router.ServeHTTP(w, r)
+	if w.Result().StatusCode != http.StatusOK {
+		t.Fail()
+	}
+}
+
+func TestMiddlewareFuncAdapter(t *testing.T) {
+	router := heligo.New()
+	router.Use(heligo.AdaptAsMiddleware(func(w http.ResponseWriter, r *http.Request) {
+		headers := w.Header()
+		if r.URL.Path == "/adapt/m1" {
+			headers["Access-Control-Allow-Origin"] = []string{"*"}
+		} else {
+			headers["Access-Control-Allow-Origin"] = r.Header["Origin"]
+		}
+	}))
+	h := func(ctx context.Context, w http.ResponseWriter, r heligo.Request) (int, error) {
+		return 200, nil
+	}
+	router.Handle("GET", "/adapt/*m", h)
+	w := httptest.NewRecorder()
+
+	r, _ := http.NewRequest("GET", "/adapt/m1", nil)
+	router.ServeHTTP(w, r)
+	if len(w.Result().Header["Access-Control-Allow-Origin"]) != 1 {
+		t.Fail()
+	}
+
+	w = httptest.NewRecorder()
+	r, _ = http.NewRequest("GET", "/adapt/m2", nil)
+	router.ServeHTTP(w, r)
+	if len(w.Result().Header["Access-Control-Allow-Origin"]) != 0 {
+		t.Fail()
+	}
 }
